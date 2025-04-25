8a036e7ee63fb1554ea011405deab10967a6315b
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-04-25 13:16:53 -0500

    feat: :sparkles: Eliminar y cambiar imagen.

--- main.js
+++ main.js
@@ -10,35 +10,16 @@ btnAgregar.addEventListener("click", () => {
     const tarjeta = document.createElement("div");
     tarjeta.className = "tarjeta";
 
-      // Nombre
-      const texto = document.createElement("p");
-      texto.textContent = nombre;
-    
-      // Añade todo a la tarjeta
-      tarjeta.appendChild(cerrar);
-      tarjeta.appendChild(img);
-      tarjeta.appendChild(texto);
-    
-      // Añade la tarjeta a la galería
-      galeria.appendChild(tarjeta);
-    
-        // Limpia los campos
-        document.getElementById("nombre").value = "";
-        document.getElementById("imagen").value = "";
-      } else {
-        alert("Por favor completa ambos campos antes de agregar.");
-      }
-
-    // Imagen del Pokemon
-    const img = document.createElement("img");
-    img.src = imagen;
-    img.alt = nombre;
-    
-    // Boton de cerrar (eliminar)
+    // Botón de cerrar (eliminar)
     const cerrar = document.createElement("button");
     cerrar.className = "cerrar";
     cerrar.textContent = "✖";
     cerrar.addEventListener("click", () => tarjeta.remove());
+
+    // Imagen del Pokémon
+    const img = document.createElement("img");
+    img.src = imagen;
+    img.alt = nombre;
 
     // Cambiar imagen al hacer clic
     img.addEventListener("click", () => {
@@ -47,4 +28,23 @@ btnAgregar.addEventListener("click", () => {
         img.src = nuevaURL;
       }
     });
+
+    // Nombre
+    const texto = document.createElement("p");
+    texto.textContent = nombre;
+
+    // Añade todo a la tarjeta
+    tarjeta.appendChild(cerrar);
+    tarjeta.appendChild(img);
+    tarjeta.appendChild(texto);
+
+    // Añade la tarjeta a la galería
+    galeria.appendChild(tarjeta);
+
+    // Limpia los campos
+    document.getElementById("nombre").value = "";
+    document.getElementById("imagen").value = "";
+  } else {
+    alert("Por favor, completa ambos campos antes de agregar.");
+  }
 });
